85b7024333d1f97158577882eb3ff4d70cd0dbee
Iaannn06 <alfianazad05@gmail.com>
2024-06-10 09:48:43 +0700

    Add project files.

---
+++ UCP2Pemdas/UCP2Pemdas.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+#include <iostream>
+#include <string>
+
+class perusahaan {
+public:
+	string nama_perusahaan;
+	void nama_perusahaan();
+};
+
+class karyawan {
+public:
+	string nama_karyawan;
+	void nama_karyawan();
+};
+
+class proyek {
+public:
+	string nama_proyek;
+	void nama_proyek();
+};
